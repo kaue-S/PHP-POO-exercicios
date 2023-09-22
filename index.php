0f2c44570c7f4360ffed0e059b371b0c1b764ef7
Kauê Evangelista <kauesilva38@hotmail.com>
2023-09-22 11:13:21 -0300

    exercício-01-finalizado

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01 POO</title>
+</head>
+<body>
+
+<h1>Exercicio 01 POO </h1>
+    <?php
+        require_once "src/livro.php";
+
+        $livro =new livro;
+
+        $livro->setTitulo("biblia");
+        $livro->setautor("diversos");
+        $livro->setPaginas(942);
+    ?>
+
+<table border="1" style="border-collapse: collapse;">
+    <thead>
+        <tr>
+            <th>
+                Livro
+            </th>
+            <th>
+                Autor
+            </th>
+            <th>
+                Paginas
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+           <td><?=$livro->getTitulo()?></td>
+           <td><?=$livro->getAutor()?></td>
+           <td><?=$livro->getPaginas()?></td>
+        </tr>
+    </tbody>
+</table>
+</body>
+</html>
